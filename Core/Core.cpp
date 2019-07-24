9d0b5571bb925401d2aec41f3d7e2c07e077b6d5
Joel Voiselle <homoprogramus@users.noreply.github.com>
2019-07-24 10:24:42 +0200

    Remove comment lines

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -40,9 +40,8 @@ void Core::loadPlugin(std::string path)
 
 	APlugin* plugin = details->initializeFunction();
 	std::string eventType = plugin->getEventType();
-	//std::vector<APlugin*>* consummers = &(_consummers.at(eventType));
-	//plugin->link(consummers);
-	int o = 0;
+	std::vector<APlugin*>* consummers = &(_consummers.at(eventType));
+	plugin->link(consummers);
 }
 
 void Core::unloadPlugins()
